c693a2ed2c6b43fe8269acb23364314caaf671c6
A-Minor15 <degtju15@outlook.jp>
2024-03-08 22:20:37 +0900

    Added app.blade.php

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    {{-- CSRF Token --}}
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    {{-- Fontawesome CDN Links --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <title>Recipeers</title>
+    {{-- Change to application name automatically --}}
+
+</head>
+<body>
+    <div class="container">
+        @yield('header')
+    </div>
+    <div class="container">
+        @yield('content')
+    </div>
+    <div class="container">
+        @yield('footer')
+    </div>
+</body>
+</html>
