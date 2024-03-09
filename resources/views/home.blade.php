e4b704d6ae37de56306f5705cc7cbe599aff0d4e
A-Minor15 <degtju15@outlook.jp>
2024-03-09 18:52:46 +0900

    Create Home Page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,95 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="home-top text-center">
+    <div class="card">
+        <img src="{{ asset('/assets/images/home_top.jpeg') }}" alt="home top image" class="home-top-img">
+        <div class="card-img-overlay d-flex">
+            <div class="m-auto">
+                <h1 class="text-success fw-bold display-1">Welcome to Recipeers!</h1>
+                <p class="top-text h5">Recipe sharing platform for Vegans, Vegetarians and people with religious dietary restrictions.</p>
+                <p class="text-success h5 mt-5">Start now for free!</p>
+                <button type="submit" class="btn btn-lg btn-warning text-white w-25 shadow">Sign Up</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="home pt-5">
+    <div class="container">
+        <div class="col">
+            <h2 class="text-success fw-bold">New Recipes</h2>
+        </div>
+        <div class="row">
+            <div class="col-8">
+                <div class="new_recipes">
+                        <div class="row">
+                            <div class="col-4 mt-3">
+                                <div class="card bg-white">
+                                    <div class="card-header">
+                                        <img src="{{ asset('/assets/images/food_sample.jpg') }}" alt="Recipe1_image" class="card-img-top">
+                                    </div>
+                                    <div class="card-body">
+                                        <div class="row">
+                                            <div class="col-8">
+                                                <span class="btn btn-sm btn-outline-warning card-tag">Vegan</span>
+                                            </div>
+                                            <div class="col-2 text-center">
+                                                <div class="row">
+                                                    <i class="fa-regular fa-bookmark"></i>
+                                                </div>
+                                                <div class="row">
+                                                    <p>1</p>
+                                                </div>
+                                            </div>
+                                            <div class="col-2 text-center">
+                                                <div class="row">
+                                                    <i class="fa-regular fa-heart"></i>
+                                                </div>
+                                                <div class="row">
+                                                    <p>1</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <h3>Recipe Title</h3>
+                                        </div>
+                                        <div class="row">
+                                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos, tempora.</p>
+                                        </div>
+                                    <button class="btn btn-warning text-white">View Detail</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="text-end">
+                        <a href="" class="text-decoration-none text-success">See more...</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="row">
+                    <a href="">
+                        <img src="{{ asset('assets/images/food_sample.jpg') }}" alt="Ad1" class="ad-image">
+                    </a>
+                </div>
+                <div class="row">
+                    <a href="">
+                        <img src="{{ asset('assets/images/food_sample.jpg') }}" alt="Ad1" class="ad-image">
+                    </a>
+                </div>
+                <div class="row">
+                    <a href="">
+                        <img src="{{ asset('assets/images/food_sample.jpg') }}" alt="Ad1" class="ad-image">
+                    </a>
+                </div>
+                <div class="row">
+                    <a href="">
+                        <img src="{{ asset('assets/images/food_sample.jpg') }}" alt="Ad1" class="ad-image">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,29 +1,99 @@
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    {{-- CSRF Token --}}
+    <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    {{-- Fontawesome CDN Links --}}
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    {{-- Bootstrap CDN --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.min.js" integrity="sha512-ykZ1QQr0Jy/4ZkvKuqWn4iF3lqPZyij9iRv6sGqLRdTPkY69YX6+7wvVGmsdBbiIfN/8OdsI7HABjvEok6ZopQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+
+    {{-- Font Varera round --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Display:ital,wght@0,100..900;1,100..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Varela+Round&display=swap" rel="stylesheet">
+
+    {{-- Font Noto Sans --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Display:ital,wght@0,100..900;1,100..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
+
+    {{-- Fontawesome CDN --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-    <title>Recipeers</title>
-    {{-- Change to application name automatically --}}
+    {{-- CSS --}}
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
 
+    <!-- Scripts -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
-    <div class="container">
-        @yield('header')
-    </div>
-    <div class="container">
-        @yield('content')
-    </div>
-    <div class="container">
-        @yield('footer')
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'Laravel') }}
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav me-auto">
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ms-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                            @if (Route::has('login'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                </li>
+                            @endif
+
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <main class="">
+            @yield('content')
+        </main>
     </div>
 </body>
 </html>
