d0ec52b9a40aaa1112473af4c29ab365623c0486
Chisato <pangori1234@yahoo.co.jp>
2024-03-14 20:08:14 +0900

    changed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -111,18 +111,11 @@
                     <small>Copyright© Kredo Recipeers</small>
                 </div>
             </div>
-<<<<<<< HEAD
     </footer>
   </div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
 </script>
 
-=======
-        </nav>
-            @yield('content')
-        </main>
-    </div>
->>>>>>> main
 </body>
 </html>
