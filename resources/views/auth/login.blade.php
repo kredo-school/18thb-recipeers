ae1c5fb1520a928ee306ef57391e84faf1b855cb
Junko <junko.katoh.inv@gmail.com>
2024-03-14 21:50:02 +0900

    Added codes about eye.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,42 @@
 @extends('layouts.app')
 
 @section('content')
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+<style>
+#checkPassword {
+  display: none;
+}
+.showText {
+  display: none;
+}
+.hideText {
+  display: inline;
+}
+.fa-eye:before {
+  display: inline;
+  margin-left: 5px; 
+}
+.fa-eye-slash:before {
+  display: none;
+}
+#checkPassword:checked + .togglePassword > .fa-eye:before {
+  display: none;
+}
+#checkPassword:checked + .togglePassword > .fa-eye-slash:before {
+  display: inline-block;
+}
+#checkPassword:checked + .togglePassword > .hideText {
+  display: none;
+}
+#checkPassword:checked + .togglePassword > .showText {
+  display: inline-block;
+}
+</style>
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -44,14 +80,17 @@
                         <div class="row mb-3">
                             <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
 
-                            <div class="col-md-6">
+
+                            <div class="col-md-6 mb-3">
+
                                 <input type="checkbox" id="checkPassword">
                                 <div class="togglePassword">
-                                  <input type="password" class="hideText" value="password123">
-                                  <input type="text" class="showText" value="password123">
-                                  <label for="checkPassword" class="fa-regular fa-eye"></label>
-                                  <label for="checkPassword" class="fa-regular fa-eye-slash"></label>
+                                  <input type="password" class="hideText form-control w-75" value="password123">
+                                  <input type="text" class="showText form-control w-75" value="password123">
+                                  <label for="checkPassword" class="fa fa-eye"></label>
+                                  <label for="checkPassword" class="fa fa-eye-slash"></label>
                                 </div>
+
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -65,7 +104,7 @@
                         <div class="row mb-3">
                             <div class="col-md-6 offset-md-4">
                                 <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                   <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
                                     <label class="form-check-label" for="remember">
                                         {{ __('Remember Me') }}
