6397c2d17f5ff92b9086d8e5caa778f357549758
Junko <junko.katoh.inv@gmail.com>
2024-03-16 15:31:14 +0900

    Deleted eye icons.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,41 +6,10 @@
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-<style>
-#checkPassword {
-  display: none;
-}
-.showText {
-  display: none;
-}
-.hideText {
-  display: inline;
-}
-.fa-eye:before {
-  display: inline;
-  margin-left: 5px; 
-}
-.fa-eye-slash:before {
-  display: none;
-}
-#checkPassword:checked + .togglePassword > .fa-eye:before {
-  display: none;
-}
-#checkPassword:checked + .togglePassword > .fa-eye-slash:before {
-  display: inline-block;
-}
-#checkPassword:checked + .togglePassword > .hideText {
-  display: none;
-}
-#checkPassword:checked + .togglePassword > .showText {
-  display: inline-block;
-}
-</style>
-
-<div class="container">
+<div class="container w-50 p-5">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
+        {{-- <div class="col-md-8"> --}}
+            {{-- <div class="card"> --}}
                 <div class=text-md-center>
                     <h1>Welcome back to Recipeers!</h1>
                 </div>
@@ -50,10 +19,10 @@
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
-                        <div class="row mb-3">
+                        <div class="row my-3 mx-auto">
                             <label for="username" class="col-md-4 col-form-label text-md-end">{{ __('Username') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-4">
                                 <input id="username" type="username" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" required autocomplete="username" autofocus>
 
                                 @error('username')
@@ -64,10 +33,10 @@
                             </div>
                         </div>
 
-                        <div class="row mb-3">
+                        <div class="row my-3 mx-auto">
                             <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-4">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -77,31 +46,7 @@
                             </div>
                         </div>
 
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-
-                            <div class="col-md-6 mb-3">
-
-                                <input type="checkbox" id="checkPassword">
-                                <div class="togglePassword">
-                                  <input type="password" class="hideText form-control w-75" value="password123">
-                                  <input type="text" class="showText form-control w-75" value="password123">
-                                  <label for="checkPassword" class="fa fa-eye"></label>
-                                  <label for="checkPassword" class="fa fa-eye-slash"></label>
-                                </div>
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-
-                            </div>
-                        </div>
-
-
-                        <div class="row mb-3">
+                        <div class="row my-3">
                             <div class="col-md-6 offset-md-4">
                                 <div class="form-check">
                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
@@ -113,9 +58,9 @@
                             </div>
                         </div>
 
-                        <div class="row mb-0">
+                        <div class="row my-3">
                             <div class="col-md-12 text-md-center">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-primary w-25 my-3">
                                     {{ __('Login') }}
                                 </button>
                                 <br>
@@ -137,8 +82,16 @@
                         </div>
                     </form>
                 </div>
-            </div>
-        </div>
+            {{-- </div> --}}
+        {{-- </div> --}}
     </div>
 </div>
+
+<div class="box">
+    <img src="{{ asset('assets/images/footer.jpg') }}" alt="footer-banner">
+    <img src="{{ asset('assets/images/footer.jpg') }}" alt="footer-banner">
+    <img src="{{ asset('assets/images/footer.jpg') }}" alt="footer-banner">
+    <img src="{{ asset('assets/images/footer.jpg') }}" alt="footer-banner">
+</div>
+
 @endsection
