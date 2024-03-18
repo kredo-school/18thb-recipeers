4c2914e7a2a9229d78f779d0b7963469216dd981
Mayuko Mita <mayuko.mita0407@gmail.com>
2024-03-18 22:13:42 +0900

    Added the right side and bottom half of the form

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,93 +1,255 @@
 @extends('layouts.app')
 
+{{-- * change title to variable --}}
+@section('title', "Recipe Title")
+
 @section('content')
-<div class="home-top text-center">
-    <div class="card">
-        <img src="{{ asset('/assets/images/home_top.jpeg') }}" alt="home top image" class="home-top-img">
-        <div class="card-img-overlay d-flex">
-            <div class="m-auto">
-                <h1 class="Welcome-text text-success fw-bold display-1">Welcome to Recipeers!</h1>
-                <p class="top-text h5">Recipe sharing platform for Vegans, Vegetarians and people with religious dietary restrictions.</p>
-                <p class="text-success h5 mt-5">Start now for free!</p>
-                <button type="submit" class="btn btn-lg btn-warning text-white w-25 shadow">Sign Up</button>
+<div class="container p-5 justify-content-center">
+    <h2 class="color1 text-center">Edit Your Profile</h2>
+    <div class="d-flex align-items-end justify-content-end my-4">
+        <a href="#" class="btn btn-sub"><i class="fa-regular fa-credit-card me-2"></i>Add Payment Information</a>
+    </div>
+
+    {{-- * add error messages to each input field --}}
+    <form action="#">
+        @csrf
+        @method('PATCH')
+
+        {{-- TOP HALF --}}
+        <div class="row">
+
+            {{-- * put variables to the old helpers and values --}}
+            {{-- LEFT SIDE --}}
+            <div class="col-6 me-5">
+                <div class="row align-items-cente mb-3">
+                    <div class="col-4 text-end">
+                        <label for="avatar" class="form-label h5 mt-1">Avatar</label>
+                    </div>
+                    <div class="col-8">
+                        <input type="file" name="image" id="image" class="form-control">
+                    </div>
+                </div>
+                <div class="row align-items-center mb-3">
+                    <div class="col-4 text-end">
+                        <label for="username" class="form-label h5 mt-1">Username</label>
+                    </div>
+                    <div class="col-8">
+                        <input type="text" name="username" id="username" class="form-control" placeholder="Username" value="">
+                    </div>
+                </div>
+                <div class="row align-items-center mb-3">
+                    <div class="col-4 text-end">
+                        <label for="email" class="form-label h5 mt-1">Email</label>
+                    </div>
+                    <div class="col-8">
+                        <input type="email" name="email" id="email" class="form-control" placeholder="user@email.com" value="">
+                    </div>
+                </div>
+                <div class="row align-items-center mb-3">
+                    <div class="col-4 text-end">
+                        <label for="password" class="form-label h5 mt-1">Password</label>
+                    </div>
+                    <div class="col-8">
+                        <input type="password" name="password" id="password" class="form-control">
+                    </div>
+                </div>
+                <div class="row align-items-center mb-3">
+                    <div class="col-4 text-end">
+                        <label for="password-confirm" class="form-label h5 mt-1">Confirm Password</label>
+                    </div>
+                    <div class="col-8">
+                        <input type="password" name="password-confirm" id="password-confirm" class="form-control">
+                    </div>
+                </div>
+                <div class="row align-items-center mb-3">
+                    <div class="col-4 text-end">
+                        <label for="birthday" class="form-label h5 mt-1">Birthday</label>
+                    </div>
+                    <div class="col-8">
+                        <input type="date" name="birthday" id="birthday" class="form-control" placeholder="YYYY/MM/DD" value="">
+                    </div>
+                </div>
+                <div class="row align-items-center mb-3">
+                    <div class="col-4 text-end">
+                        <label for="gender" class="form-label h5 mt-1">Gender</label>
+                    </div>
+                    <div class="col-8">
+                        <select class="form-select" name="gender" id="gender" class="form-control">
+                            <option value="" selected>Select your gender</option>
+                            <option value="female">Female</option>
+                            <option value="male">Male</option>
+                            <option value="neither chosen">Prefer not to say</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="row align-items-center mb-3">
+                    <div class="col-4 text-end">
+                        <label for="eating_pref" class="form-label h5 mt-1">Eating Preference</label>
+                    </div>
+                    <div class="col-8">
+                        <select class="form-select" name="eating_pref" id="eating_pref">
+                            {{-- * if preference is null null --}}
+                            <option value="" selected>Choose your preference</option>
+                            {{-- * else pre-select an option --}}
+                            <option value="vegan">Vegan</option>
+                            <option value="vegetarian">Vegetarian</option>
+                            <option value="halal">Halal</option>
+                            <option value="hindi">Hindi Diet</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+            {{-- RIGHT SIDE --}}
+            <div class="col">
+                <div class="row align-items-center mb-3">
+                    <div class="col-4 text-end">
+                        <label for="nationality" class="form-label h5 mt-1">Nationality</label>
+                    </div>
+                    <div class="col-8">
+                        {{-- * get the list of the countries --}}
+                        <select class="form-select" name="nationality" id="nationality" class="form-control" value="">
+                            <option value="" selected>Select your nationality</option>
+                            <option value="Japan">Japan</option>
+                            <option value="Canada">Canada</option>
+                            <option value="New Zealand">New Zealand</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="row align-items-center mb-3">
+                    <div class="col-4 text-end">
+                        <label for="residence_city" class="form-label h5 mt-1">City of Residence</label>
+                    </div>
+                    <div class="col-8">
+                        {{-- * get the list of the major cities --}}
+                        <select class="form-select" name="residence_city" id="residence_city" class="form-control">
+                            <option value="" selected>Select your city of residence</option>
+                            <option value="Tokyo">Tokyo</option>
+                            <option value="Ottawa">Ottawa</option>
+                            <option value="Wellington">Wellington</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="row align-items-center mb-3">
+                    <div class="col-4 text-end">
+                        <label for="job_status" class="form-label h5 mt-1">Job Status</label>
+                    </div>
+                    <div class="col-8">
+                        {{-- * get the list of the major cities --}}
+                        <select class="form-select" name="job_status" id="job_status" class="form-control">
+                            <option value="" selected>Select your job status</option>
+                            <option value="Full-time">Full-time</option>
+                            <option value="Part-time">Part-time</option>
+                            <option value="Business Owner">Business Owner</option>
+                            <option value="Self-employed">Self-employed</option>
+                            <option value="Internship">Internship</option>
+                            <option value="Student">Student</option>
+                            <option value="Other">None of the above</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="row align-items-center mb-4">
+                    <div class="col-4 text-end">
+                        <label for="introduction" class="form-label h5 mt-1">Introduction</label>
+                    </div>
+                    <div class="col-8">
+                        <textarea name="introduction" id="introduction" rows="8" class="form-control" placeholder="Please introduce yourself."></textarea>
+                    </div>
+                </div>
+                <div class="row align-items-center mb-3">
+                    <div class="col-10 text-end">
+                        <label for="business_info" class="form-label">Do you want to switch to a business account?</label>
+                    </div>
+                    <div class="col-2 mb-2 text-end">
+                        <input type="checkbox" name="business_info" id="business_info" class="form-check-input me-1" onchange="toggleBusinessInfo()"> Yes
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-</div>
-
-<div class="home pt-5">
-    <div class="container">
-        <div class="row">
-            <h1 class="text-success fw-bold">New Recipes</h1>
-        </div>
-        <div class="row">
-            {{-- cards index --}}
-            <div class="col-lg-8 col-12">
-                <div class="row d-flex flex-wrap">
-
-                    {{-- Card --}}
-                    <div class="col">
-                        <div class="card bg-white d-flex flex-column col-12 col-sm-12 col-md-6 col-xl-4">
-
-                            {{-- Card Header --}}
-                            <div class="card-header">
-                                <img src="{{ asset('/assets/images/food_sample.jpg') }}" alt="" class="w-100">
+        {{-- BOTTOM HALF --}}
+        {{-- * when the user clicks the checkbox as the business account --}}
+        <div id="businessInfoContainer" style="display: none;">
+            <hr class="color2 my-5">
+            {{-- BUSINESS INFORMATION --}}
+            <div class="row">
+                <h3 class="color1 mb-5 text-center">Business Information</h3>
+                <div class="row">
+                    {{-- LEFT SIDE --}}
+                    <div class="col-6">
+                        <div class="row align-items-center mb-3">
+                            <div class="col-4 text-end">
+                                <label for="business_name" class="form-label h5 mt-1">Business Name</label>
                             </div>
-
-                            {{-- Card Body --}}
-                            <div class="card-body">
-                                <div class="row">
-
-                                    {{-- Tag --}}
-                                    <div class="col-10 col-sm-10 col-md-9">
-                                        <span class="badge border border-success rounded-pill text-success">Vegan</span>
-                                    </div>
-
-                                    {{-- Bookmark --}}
-                                    <div class="col-1">
-                                        <span class="fa-layers">
-                                            <i class="fa-regular fa-bookmark"></i><br>
-                                            <span class="fa-layers-counter">1</span>
-                                        </span>
-                                    </div>
-
-                                    {{-- Heart --}}
-                                    <div class="col-1">
-                                        <span class="fa-layers">
-                                            <i class="fa-regular fa-heart"></i><br>
-                                            <span class="fa-layers-counter">1</span>
-                                        </span>
-                                    </div>
-
-                                </div>
-
-                                {{-- Recipe Title --}}
-                                <div class="row">
-                                    <h3>Recipe Title</h3>
-                                </div>
-
-                                {{-- Recipe Over View --}}
-                                <div class="row">
-                                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex, tempora.</p>
-                                </div>
-
-                                <div class="row">
-                                    <div class="col">
-                                        <button type="button" class="btn btn-warning">View Details</button>
-                                    </div>
-                                </div>
+                            <div class="col-8">
+                                <input type="text" name="business_name" id="business_name" class="form-control" placeholder="Name of your company/restaurant" value="">
+                            </div>
+                        </div>
+                        <div class="row align-items-center mb-3">
+                            <div class="col-4 text-end">
+                                <label for="location" class="form-label h5 mt-1">Location</label>
+                            </div>
+                            <div class="col-8">
+                                <input type="text" name="location" id="location" class="form-control" placeholder="Location" value="">
+                            </div>
+                        </div>
+                        <div class="row align-items-center mb-3">
+                            <div class="col-4 text-end">
+                                <label for="pic" class="form-label h5 mt-1">Person in Charge</label>
+                            </div>
+                            <div class="col-8">
+                                <input type="text" name="pic" id="pic" class="form-control" placeholder="Name of person in charge" value="">
                             </div>
                         </div>
                     </div>
-
+                    {{-- RIGHT SIDE --}}
+                    <div class="col-6">
+                        <div class="row align-items-center mb-3">
+                            <div class="col-4 text-end">
+                                <label for="hp_url" class="form-label h5 mt-1">Website</label>
+                            </div>
+                            <div class="col-8">
+                                <input type="url" name="hp_url" id="hp_url" class="form-control" placeholder="URL of your business website" value="">
+                            </div>
+                        </div>
+                        <div class="row align-items-center mb-3">
+                            <div class="col-4 text-end">
+                                <label for="delivery_url" class="form-label h5 mt-1">Delivery Service</label>
+                            </div>
+                            <div class="col-8">
+                                <input type="url" name="delivery_url" id="delivery_url" class="form-control" placeholder="Link to your delivery service webpage" value="">
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-
-            {{-- Ads index --}}
-            <div class="col-12 col-lg-4 mt-5 mt-md-2">
-                <img src="{{ asset('/assets/images/food_sample.jpg') }}" alt="" class="w-100">
+        </div>
+        {{-- BUTTONS --}}
+        <div class="row justify-content-center mt-5">
+            <div class="col-2 d-flex justify-content-between">
+                <button class="btn btn-sub flex-grow-1">Cancel</button>
+            </div>
+            <div class="col-2 d-flex justify-content-between">
+                <input type="submit" value="Save" class="btn btn-main flex-grow-1">
             </div>
         </div>
-    </div>
+    </form>
+    
+</div>
+<script>
+    function toggleBusinessInfo() {
+        var container = document.getElementById('businessInfoContainer');
+        var checkbox = document.getElementById('business_info');
+        
+        if (checkbox.checked) {
+            container.style.display = 'block';
+        } else {
+            container.style.display = 'none';
+        }
+    }
+</script>
+<div class="box">
+    <img src="{{ asset('assets/images/footer.jpg') }}" alt="footer-banner">
+    <img src="{{ asset('assets/images/footer.jpg') }}" alt="footer-banner">
+    <img src="{{ asset('assets/images/footer.jpg') }}" alt="footer-banner">
+    <img src="{{ asset('assets/images/footer.jpg') }}" alt="footer-banner">
 </div>
 @endsection
