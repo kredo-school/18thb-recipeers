dd61e02fd784aecf963078c776af16fb7d09ad38
TomoeKinoshita <tomoek.ori@gmail.com>
2024-03-19 22:33:03 +0900

    paymentInfo fixed (added one more bottom image)

--- resources/views/users/account/paymentInfo.blade.php
+++ resources/views/users/account/paymentInfo.blade.php
@@ -65,6 +65,7 @@
     <img src="{{ asset('assets/images/footer.jpg') }}" alt="footer-banner">
     <img src="{{ asset('assets/images/footer.jpg') }}" alt="footer-banner">
     <img src="{{ asset('assets/images/footer.jpg') }}" alt="footer-banner">
+    <img src="{{ asset('assets/images/footer.jpg') }}" alt="footer-banner">
 </div>
 
 @endsection
