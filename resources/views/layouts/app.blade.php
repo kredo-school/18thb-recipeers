21bb59dcc3c7ab8cf18ac927df768313b78a7015
Mayuko Mita <mayuko.mita0407@gmail.com>
2024-03-22 21:04:43 +0900

    Solved conflicts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
 <!-- CSRF Token -->
 <meta name="csrf-token" content="{{ csrf_token() }}">
 
-<title>{{ config('app.name', 'Laravel') }} | @yield('title')</title>
+<title> @yield('title') | {{ config('app.name', 'Laravel') }}</title>
 
 {{-- google font --}}
 <link rel="preconnect" href="https://fonts.googleapis.com">
