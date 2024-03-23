baf0b0f70b90c4d556739b19878e01368b53ee9c
TomoeKinoshita <tomoek.ori@gmail.com>
2024-03-23 10:47:35 +0900

    edit-account-delete completed

--- resources/views/modals/edit-account-delete.blade.php
+++ resources/views/modals/edit-account-delete.blade.php
@@ -1,83 +1,37 @@
-{{-- still editing --}}
+{{-- Delete an Account --}}
+{{-- * Change to variables --}}
 
-<!doctype html>
-<html lang="en">
-    <head>
-        <title></title>
+<div class="modal fade" id="accountTrashModal">
+    {{-- -{{ $user->id }} --}}
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header d-flex justify-content-center align-items-center">
+                <h3 class="modal-title color1">
+                    <i class="fa-solid fa-user-xmark me-3"></i>
+                    Delete Account
+                </h3>
+            </div>
+            <div class="horizontal-line3"></div>
+            <div class="horizontal-line4"></div>
 
-        <!-- Required meta tags -->
-        <meta charset="utf-8" />
-        <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, shrink-to-fit=no"
-        />
-
-        <!-- Bootstrap CSS v5.2.1 -->
-        <link
-            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
-            rel="stylesheet"
-            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
-            crossorigin="anonymous"
-        />
-
-        <!-- font-awesome -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    </head>
-
-    <body>
-        <div class="modal fade" id="delete-account-id" aria-labelledby="accountDeleteModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-
-                    <div class="modal-header">
-                        <h2 class="modal-title h5 color1" id="accountDeleteModal">
-                            <i class="fa-solid fa-user-xmark"></i>
-                            Delete Account
-                        </h2>
-                        <!-- <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button> -->
-
-                        <div class="horizontal-line2"></div>
-                        <div class="horizontal-line3"></div>
-                        <div class="horizontal-line4"></div>
+            <div class="modal-body justify-items-center py-5 text-center">
+                <p class="fw-light mb-5">You are about to delete your account.</p>
+                <img src="../assets/images/user.jpg" alt="avatar" class="rounded-circle img-lg mb-3">
+                <p class="h4 fw-light">Username</p>
+            </div>
+            <div class="modal-footer border-0 d-flex justify-content-center mb-5">
+                {{-- <form action="{{ route('account.users.delete',$user->id) }}" method="post">
+                    @csrf
+                    @method('DELETE') --}}
+                        <button type="button" class="btn btn-sub btn-block w-25 me-4" data-bs-dismiss="modal">
+                            Cancel
+                        </button>
+                        <button type="submit" class="btn btn-main btn-block w-25">
+                            Delete
+                        </button>
                     </div>
-
-                    <div class="modal-body">
-                        <p>You are about to delete your account.</p>
-                    </div>
-
-                    <!-- Avatar -->
-                    <div class="col-auto">
-                        <i class="fa-solid fa-circle-user icon-lg color1" class="icon-md"></i>
-                        <p class="text-center">Username</p>
-                    </div>
-
-                    <!-- buttons -->
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-sub" data-bs-dismiss="modal">Cancel</button>
-                        <button type="button" class="btn btn-main">Delete</button>
-                    </div>
-
-                    <!-- bottom image -->
-                    <div class="box">
-                        <img src="{{ asset('assets/images/footer.jpg') }}" alt="footer-banner">
-                        <img src="{{ asset('assets/images/footer.jpg') }}" alt="footer-banner">
-                        <img src="{{ asset('assets/images/footer.jpg') }}" alt="footer-banner">
-                        <img src="{{ asset('assets/images/footer.jpg') }}" alt="footer-banner">
-                    </div>
-                </div>
+                {{-- </form> --}}
             </div>
         </div>
-
-        <script
-            src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
-            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
-            crossorigin="anonymous"
-        ></script>
-
-        <script
-            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
-            integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
-            crossorigin="anonymous"
-        ></script>
-    </body>
-</html>
+    </div>
+</div>
