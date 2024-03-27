859cc475a5eab5243cc5f1cbda406b31c91566df
Mayuko Mita <mayuko.mita0407@gmail.com>
2024-03-27 20:08:04 +0900

    Made the backend for the inquiry form

--- app/Http/Controllers/InquiryController.php
+++ app/Http/Controllers/InquiryController.php
@@ -3,8 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Inquiry;
 
 class InquiryController extends Controller
 {
-    //
+    private $inquiry;
+
+    public function __construct(Inquiry $inquiry){
+        $this->inquiry = $inquiry;
+    }
+
+    public function create(Request $request){
+        $request->validate([
+            'title' => 'required|min:1',
+            'body' => 'required|min:1',
+            'name' => 'required|min:1',
+            'email' => 'required|email|unique:inquiries,email',
+            'agreement' => 'required|accepted'
+        ]);
+
+        $this->inquiry->title = $request->title;
+        $this->inquiry->body = $request->body;
+        $this->inquiry->inquirer_name = $request->name;
+        $this->inquiry->email = $request->email;
+        $this->inquiry->save();
+
+        return redirect()->route('home');
+    }
 }
